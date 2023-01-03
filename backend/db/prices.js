19d07f14331b11b0c2a209efa0b7981eac2b82cc
gregorvolcanjk <gv0075@student.uni-lj.si>
2023-01-03 15:18:50 +0100

    Neki dela

--- backend/db/prices.js
+++ backend/db/prices.js
@@ -1,41 +1,89 @@
 const knex = require('./knex');
 
 function getElAllPrices() {
-	return knex('electricity_price_user').where("geo","AL").andWhere('indic_en','MSHH').andWhere('year',2015).select('geo','year','value');
+    return knex('electricity_price_user').where("geo", "AL").andWhere('indic_en', 'MSHH').andWhere('year', 2015).select('geo', 'year', 'value');
 }
 
-function getElCountryPrice(household,geo,year) {
-	return knex('electricity_price_user').where("geo",geo).andWhere('indic_en',household).andWhere('year',year).select('geo','year','value');
+function getElCountryPrice(household, geo, year) {
+    return knex('electricity_price_user').where("geo", geo).andWhere('indic_en', household).andWhere('year', year).select('geo', 'year', 'value');
+}
+
+function getAllPrice(geo) {
+    return knex('electricity_price_user').where("geo", geo).andWhere('indic_en', 'MSHH').select('year', 'value');
+}
+
+function getAllGasPrice(geo) {
+    return knex('gas_price_user').where("geo", geo).andWhere('indic_en', 'MSHH').select('year', 'value');
 }
 
 function getElYearPrices(household, year) {
-	return knex('electricity_price_user').where("indic_en",household).andWhere('year',year).select('geo','year','value');
+    return knex('electricity_price_user').where("indic_en", household).andWhere('year', year).select('geo', 'year', 'value');
+}
+function getMaxElPrice(household, geo) {
+    return knex('electricity_price_user').where("geo", geo).andWhere('indic_en', household).max('value').select('geo', 'year', 'value');
+}
+function getMinElPrice(household, geo) {
+    return knex('electricity_price_user').where("geo", geo).andWhere('indic_en', household).min('value').select('geo', 'year', 'value');
 }
 
-function getElAllCountryPrices(household,geo) {
-	return knex('electricity_price_user').where("geo",geo).andWhere('indic_en',household).select('geo','year','value');
+function getMaxGasPrice(household, geo) {
+    return knex('gas_price_user').where("geo", geo).andWhere('indic_en', household).max('value').select('geo', 'year', 'value');
+}
+function getMinGasPrice(household, geo) {
+    return knex('gas_price_user').where("geo", geo).andWhere('indic_en', household).min('value').select('geo', 'year', 'value');
 }
 
-function getGasCountryPrice(household,geo,year) {
-	return knex('gas_price_user').where("geo",geo).andWhere('indic_en',household).andWhere('year',year).select('geo','year','value');
+function getElAllCountryPrices(household, geo) {
+    return knex('electricity_price_user').where("geo", geo).andWhere('indic_en', household).select('geo', 'year', 'value');
+}
+
+function getGasCountryPrice(household, geo, year) {
+    return knex('gas_price_user').where("geo", geo).andWhere('indic_en', household).andWhere('year', year).select('geo', 'year', 'value');
 }
 
 function getGasYearPrices(household, year) {
-	return knex('gas_price_user').where("indic_en",household).andWhere('year',year).select('geo','year','value');
+    return knex('gas_price_user').where("indic_en", household).andWhere('year', year).select('geo', 'year', 'value');
 }
 
 function getGasByCountry(household, geo) {
-	return knex('gas_price_user').where("indic_en",household).andWhere('geo',geo).select('geo','year','value')
+    return knex('gas_price_user').where("indic_en", household).andWhere('geo', geo).select('geo', 'year', 'value')
+}
+
+// Values of fuels used
+function getFuelName(siec) {
+    return knex('fuel_types').where('siec',siec)
+}
+
+function getHouseholdUsageCountryFuel(geo, fuel) {
+    return knex('household_by_fuel').where("geo", geo).andWhere('siec', fuel).select('geo', 'year', 'value')
+}
+
+function getHouseholdUsageYearFuel(year, fuel) {
+    return knex('household_by_fuel').where("year", year).andWhere('siec', fuel).select('geo', 'year', 'value')
+}
+
+function getHouseholdUsageCountryYear(geo, year) {
+    return knex('household_by_fuel').join('fuel_types','fuel_types.siec','household_by_fuel.siec').where("geo", geo).andWhere('year', year).select('fuel_types.fuel','geo', 'value')
 }
 
 
 
 module.exports = {
-	getElAllPrices,
-	getElCountryPrice,
-	getElYearPrices,
-	getElAllCountryPrices,
-	getGasCountryPrice,
-	getGasYearPrices,
-	getGasByCountry
+    getElAllPrices,
+    getElCountryPrice,
+    getElYearPrices,
+    getElAllCountryPrices,
+    getGasCountryPrice,
+    getGasYearPrices,
+    getGasByCountry,
+    getAllPrice,
+    getAllGasPrice,
+    getFuelName,
+    getHouseholdUsageCountryFuel,
+    getHouseholdUsageYearFuel,
+    getHouseholdUsageCountryYear,
+    getMaxElPrice,
+    getMinElPrice,
+    getMaxGasPrice,
+    getMinGasPrice
 };
